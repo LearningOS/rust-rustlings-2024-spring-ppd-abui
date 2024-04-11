26d447725e15eec2ac740012a9044bd28ac1d29e
caizhengwei <1765002006@qq.com>
2024-04-11 13:28:58 +0800

    quiz1 quiz2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: i32) -> i32 {
+    let res = if x > 40 { x } else { 2 * x };
+    res
+}
 
 // Don't modify this function!
 #[test]
